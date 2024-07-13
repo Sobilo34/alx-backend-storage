069ab040b6c6cfd40c9275063313531dfc6ab37f
Sobilo34 <bilalsolih60@gmail.com>
2024-07-13 04:27:11 +0100

    11-need_meeting.sql

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,10 @@
+-- Creates a view named need_meeting that lists all students who need a meeting.
+DROP VIEW IF EXISTS need_meeting;
+
+CREATE VIEW need_meeting AS 
+SELECT name FROM students 
+WHERE 
+    score < 80 AND 
+    (last_meeting IS NULL 
+        OR 
+    last_meeting < ADDDATE(CURDATE(), interval -1 MONTH));
