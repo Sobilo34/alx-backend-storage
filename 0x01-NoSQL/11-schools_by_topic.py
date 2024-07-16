7465f39bfbe97f04d1815906d52e2f26aa9fba3f
Sobilo34 <bilalsolih60@gmail.com>
2024-07-16 15:38:35 -0700

    used the list method to list the schools

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 """
-A Python function that returns the list of school having a specific topic
+A Python function that returns the list of schools having a specific topic
 """
-
 
 def schools_by_topic(mongo_collection, topic):
     """
-    returns the list of school
-    """
-    new_schools = []
-    school = mongo_collection.find({ "topic" : topic })
-    for sch in school:
-        new_schools.append(sch)
+    Returns the list of schools having a specific topic.
 
-    return new_schools
+    Args:
+    mongo_collection (pymongo.collection.Collection): The MongoDB collection.
+    topic (str): The topic to search for.
+
+    Returns:
+    list: A list of schools that have the specific topic.
+    """
+    # Assuming the field name in the MongoDB documents is 'topics'
+    return list(mongo_collection.find({"topics": topic}))
+
